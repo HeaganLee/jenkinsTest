b79d9ef1281cb66a1c1d528d5a314a912b35be3e
LeeHeaKang <ekzmfkdl12@gmail.com>
2025-11-11 14:35:31 +0900

    경로 수정

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    
+    @GetMapping("/")
     public ResponseEntity<?> test() {
 
         return ResponseEntity.ok().body("test2");
